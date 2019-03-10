69b6dd0e50d70f68556d794565e8df6835954062
Tim Snow <Tim.snow1991@gmail.com>
2019-03-10 22:15:34 +0000

    set up really important tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16.3'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+beforeAll(() => {
+  Enzyme.configure({ adapter: new Adapter() })
+})
+
+it('looks sick', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+  expect(shallow(<App />)).toMatchSnapshot()
+})
