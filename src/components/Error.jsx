1a26683181a7d989382eee101149a18ffa4f6783
Tim Snow <Tim.snow1991@gmail.com>
2019-03-25 21:57:47 +0000

    functional error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 import ErrorImg from '../assets/error.png'
 
-function Error() {
-  return (
-    <div>
-      <h2>Oh no! Something's gone wrong!</h2>
-      <img
-        style={{ width: 300 }}
-        src={ErrorImg}
-        alt="illustration by Ouch.pics https://icons8.com"
-      />
-      <p>It works on my machine...</p>
-    </div>
-  )
-}
+const Error = () => (
+  <div>
+    <h2>Oh no! Something's gone wrong!</h2>
+    <img
+      style={{ width: 300 }}
+      src={ErrorImg}
+      alt="illustration by Ouch.pics https://icons8.com"
+    />
+    <p>It works on my machine...</p>
+  </div>
+)
 
 export default Error
